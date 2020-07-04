3dbbaa7ca635936803782b4ecbcca5bbbdf4e039
Anthony Samartsev <ant.samarcev@gmail.com>
2020-07-04 20:30:29 +0300

    +check total amount of KEYS

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ def FUNC_exit_with_error(error_code):
     print("Exit...")
     sys.exit()
 def FUNC_read_file(filename):
-    file = open(filename, 'r')
+    file = open(filename, 'r', encoding='utf-8')
     data = file.readlines()
     file.close()
     return data
@@ -23,11 +23,11 @@ def FUNC_find_substring(substring, string):
         return 0
     else:
         return index
-def FUNC_find_substring_return_after(substring, string):
+def FUNC_find_substring_return_after(substring, string, remove_newline):                # string, string, int(0/1)
     index = FUNC_find_substring(substring, string)
     if index:
         cut_from = index+len(substring)
-        return string[cut_from:]
+        return string[cut_from:len(string)-1*remove_newline]
     else:
         return ""
 def FUNC_find_substring_return_before(substring, string):
@@ -53,13 +53,24 @@ NEW_filename = temp+" formatted.csv"
 print("Making the first few mandatory lines...")
 
 for string in DATA_original:
-    tempTEXT = FUNC_find_substring_return_after("string m_Name = ", string)
+    tempTEXT = FUNC_find_substring_return_after("string m_Name = ", string, 1)
     if tempTEXT:
-        DATA_new = ['Key,' + tempTEXT[1:-2] + ',NOTES,' + NEW_filename + ',\n',
+        DATA_new = ['Key,' + tempTEXT[1:-1] + ',NOTES,' + NEW_filename + ',\n',
                     ',,,,,,,,,,,,,,,,,,,\n',
                     'UseCyrillicFont,No,"Пометка для переводчиков: поставьте на позиции (между разделителями) вашего языка «Yes», если используете кириллицу",Yes,\n',
                     ',,,,,,,,,,,,,,,,,,,\n']
         break
+
+# check total amount of KEYS
+temp_counter = False
+for string in DATA_original:
+    tempTEXT = FUNC_find_substring_return_after("int size = ", string, 1)
+    if tempTEXT:
+        if temp_counter:
+            print("KEYS total....................."+tempTEXT)
+            break
+        else:
+            temp_counter = True
 
 # finalizing
 FUNC_write_to_the_file(DATA_new, NEW_filename)
